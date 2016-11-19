192c59741a10004de9fb36f37978a85aa0ddef5e
khmacdonald <kh_macdonald@yahoo.com>
2016-11-18 21:22:19 -0500

    Adding p62

--- euler/utilities/my_euler.py
+++ euler/utilities/my_euler.py
@@ -7,6 +7,22 @@ import sys
 av = sys.argv
 ac = len(av)
 
+def next_lex ( a ):
+    i = len(a)-2
+    while not ( i<0 or a[i] < a[i+1]):
+        i = i-1
+    if i<0:
+        return False
+
+    j = len(a)-1
+    while not ( a[j]>a[i] ):
+        j = j-1
+    tmp = a[i]
+    a[i] = a[j]
+    a[j] = tmp
+    a[i+1:] = reversed(a[i+1:])
+    return True
+
 
 
 if __name__=='__main__':
