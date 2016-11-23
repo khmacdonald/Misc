d7c2ed4547674ee12d034c4294b948a76ec08507
khmacdonald <kh_macdonald@yahoo.com>
2016-11-23 00:45:39 -0500

    p92

--- euler/p0092.py
+++ euler/p0092.py
@@ -11,51 +11,44 @@ import my_euler
 av = sys.argv
 ac = len(av)
 
-df = {0:1, 1:1, 2:2, 3:6, 4:24, 5:120, 6:720, 7:5040, 8:40320, 9:362880}
 
 def print_project_answer( pnum, answer ):
     project_output = "Project Euler %d answer is" % pnum
     print("")
     print("%s = %d" % (project_output,answer))
 
-def dig_fac ( n ):
+def sq_sum ( n ):
     sm = 0
     while n:
         d = n % 10
         n = n / 10
-        sm = sm + df[d]
+        sm = sm + (d*d)
     return sm
 
-def cycle_cnt ( n ):
-    l = [n]
-    x = n
-    cnt = 1
-    while True:
-        y = dig_fac(x)
-        if y in l:
-            break
-        l.append(y)
-        cnt = cnt + 1
-        x = y
-    return cnt
-    
 def test():
-    y = 69
+    y = 44
     if 2==ac:
         y = int(av[1])
-    ln = cycle_cnt(y)
+    x = sq_sum(y)
+    print("%d -> %d" % (y,x))
 
 def proj():
     ans = 0
-    upper = 10**6
-    cnt = 0
-    for x in range(2,upper):
-        sys.stdout.write("  x = %d\r" % x)
-        if 60==cycle_cnt(x):
-            cnt = cnt+1
 
-    ans = cnt
-    print_project_answer(74,ans)
+    upper = 10**7
+    print("Starting")
+    for n in range(2,upper):
+        sys.stdout.write("n = %d\r" % n)
+        x = n
+        while True:
+            x = sq_sum(x)
+            if 1==x:
+                break
+            if 89==x:
+                ans = ans + 1
+                break
+
+    print_project_answer(92,ans)
 
 
 if __name__=='__main__':
