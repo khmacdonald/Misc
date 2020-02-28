a2e9a5187bc90f4f5b1076786f9ce4f272708ed9
Kenneth MacDonlad <kh_macdonald@yahoo.com>
2020-02-28 15:35:52 -0500

    Adding more work in Chapter 2.

--- artificial_intelligence_with_python_joshi/chapter02/labels.py
+++ artificial_intelligence_with_python_joshi/chapter02/labels.py
@@ -3,29 +3,27 @@
 import numpy as np
 from sklearn import preprocessing
 
-input_data = np.array([[5.1, -2.9, 3.3],
-                       [-1.2, 7.8, -6.1],
-                       [3.9, 0.4, 2.1],
-                       [7.3, -9.9, -4.5]])
+# Sample input labels
+input_labels = [ 'red', 'black', 'red', 'green', 'black', 'yellow', 'white' ]
 
-# Binarization
-data_binarized = preprocessing.Binarizer(threshold=2.1).transform(input_data)
-print("\nBinarized data: \n",data_binarized)
+# Create label encoder and fit the labels
+encoder = preprocessing.LabelEncoder()
+encoder.fit(input_labels)
 
-# Print mean and standar deviation
-print("\nBEFORE: ")
-print("Mean =", input_data.mean(axis=0))
-print("Std deviation =", input_data.std(axis=0))
+# Print the mapping
+print("\nLabel mapping:")
+for i, item in enumerate(encoder.classes_):
+    print(item, "--->", i)
+
+# Encode a set of labels using the encoder
+test_labels = [ 'green', 'red', 'black' ]
+encoded_values = encoder.transform(test_labels)
+print("\nLabels =", test_labels)
+print("Encoded values =", encoded_values)
 
 
-# Remove mean
-data_scaled = preprocessing.scale(input_data)
-print("\nAFTER: ")
-print("Mean =", data_scaled.mean(axis=0))
-print("Std deviation =", data_scaled.std(axis=0))
-
-# Min Max Scaling
-data_scaler_minmax = preprocessing.MinMaxScaler(feature_range=(0,1))
-data_scaled_minmax = data_scaler_minmax.fit_transform(input_data)
-print("\nMin max scaled data: \n",data_scaled_minmax)
-
+# Decode a set of values using the encoder
+encoded_values = [3, 0, 4, 1]
+decoded_list = encoder.inverse_transform(encoded_values)
+print("\nEncoded values =", encoded_values)
+print("Decoded values =",list(decoded_list))
