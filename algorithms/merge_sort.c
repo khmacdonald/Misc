888cc7ed663894ed62d6dc67e359956d00d2c998
Kenneth MacDonald <kh_macdonald@yahoo.com>
2020-03-14 13:24:28 -0400

    Working merge sort.

--- algorithms/merge_sort.c
+++ algorithms/merge_sort.c
@@ -8,14 +8,19 @@ gcc merge_sort.c -o merge_sort ../math/brand.o
 #include <string.h>
 
 #include "../math/brand.h"
+#include "../common/print_statements.h"
 
 /* 
  * Testing the merge sort with a basic integer array 
  */
 
+ int level = 0;
+
 void merge_sort ( int * arr, int n );
 void print_int_array ( int * arr, int n, int new_line );
 void rnd_arr ( int * arr, int n );
+
+/* ------------------------------------------------------------ */
 
 int main ( int argc, char ** argv ) {
     int n = 10;
@@ -39,15 +44,20 @@ int main ( int argc, char ** argv ) {
     return 0;
 }
 
+/* ------------------------------------------------------------ */
+
 void rnd_arr ( int * arr, int n ) {
     int k;
     int mod = 1000;
     brand_t ctx;
-    brand_seed_init(ctx);
-    brand_array((uint32_t*)arr,n,ctx);
+
+    brand_seed_init(ctx);               /* Seed and initialize randomizer */
+    //brand_init(ctx,0xbadbeef);        /* Initialize with a fixed seed */
+    brand_array((uint32_t*)arr,n,ctx);  /* Get random integer array */
+
     for ( k=0; k<n; ++k ) {
-        arr[k] &= 0x7fffffff;
-        arr[k] = arr[k] % mod;
+        arr[k] &= 0x7fffffff;       /* Ensure positive numbers */
+        arr[k] = arr[k] % mod;      /* Modulo numbers */
     }
 
     return;
@@ -55,9 +65,15 @@ void rnd_arr ( int * arr, int n ) {
 
 void print_int_array ( int * arr, int n, int new_line ) {
     int k;
-    for ( k=0; k<n; ++k ) {
-        printf("%d ",arr[k]);
+
+    if ( n<1 ) {
+        return;
     }
+    printf("(%d",arr[0]);
+    for ( k=1; k<n; ++k ) {
+        printf(", %d",arr[k]);
+    }
+    printf(")");
     if ( new_line ) {
         printf("\n");
     }
@@ -66,6 +82,7 @@ void print_int_array ( int * arr, int n, int new_line ) {
 void merge ( int * arr, int n, int * low, int ln, int * high, int hn ) {
     int lidx=0, hidx=0,k;
 
+    /* Merge arrays */
     for ( k=0; k<n && lidx<ln && hidx<hn; ++k ) {
         if ( low[lidx] < high[hidx] ) {
             arr[k] = low[lidx++];
@@ -74,48 +91,63 @@ void merge ( int * arr, int n, int * low, int ln, int * high, int hn ) {
         }
     }
 
+    /* Copy the remainder of the low or high array */
     if ( lidx<ln ) {
-        for ( ; k<n && lidx<ln; ++k, ++lidx ) {
-            arr[k] = low[lidx];
+        for ( ; k<n && lidx<ln; ++k ) {
+            arr[k] = low[lidx++];
         }
-    } else if ( lidx<ln ) {
-        for ( ; k<n && hidx<ln; ++k, ++hidx ) {
-            arr[k] = low[hidx];
+    } else if ( hidx<hn ) {
+        for ( ; k<n && hidx<hn; ++k ) {
+            arr[k] = high[hidx++];
         }
     }
 }
 
-void merge_sort ( int * arr, int n ) {
-    int half,tmp,k;
-    int ln,hn;
-    int * low = NULL;
-    int * high = NULL;
-
-    if ( 1==n ) {
-        return;
-    } else if ( 2==n ) {
+void merge_sort_base ( int * arr, int n ) {
+    int tmp;
+    if ( 2==n ) {
         if ( arr[0]>arr[1] ) {
             tmp = arr[0];
             arr[0] = arr[1];
             arr[1] = tmp;
         }
     }
-    half = n/2;
+    return;
+}
 
-    ln = half;
-    low = calloc(ln,sizeof(*low));
-    memcpy(low,arr,ln*sizeof(*low));
+int * copy_part_of_array ( int * arr, int n ) {
+    int * narr = calloc1(n,sizeof(*narr));
+    memcpy(narr,arr,n*sizeof(narr));
+    return narr;
+}
 
-    hn = n-half;
-    high= calloc(hn,sizeof(*high));
-    memcpy(high,arr+half,hn*sizeof(*high));
+void merge_sort ( int * arr, int n ) {
+    int half,ln,hn;
+    int * low = NULL;
+    int * high = NULL;
 
-    merge_sort(low,ln);
-    merge_sort(high,hn);
-    merge(arr,n,low,ln,high,hn);
+    level++;    /* Level of recursion */
+
+    if ( n<3 ) {
+        merge_sort_base(arr,n);
+        goto END;
+    }
+
+    /* Divide and conquor */
+    ln = n/2;
+    hn = n-ln
+
+    low  = copy_part_of_array(arr,ln);
+    high = copy_part_of_array(arr+half,hn);
+
+    merge_sort(low,ln);             /* Sort low half of array */
+    merge_sort(high,hn);            /* Sort high half of array */
+    merge(arr,n,low,ln,high,hn);    /* Merge sorted halves */
 
     free(low);
     free(high);
 
+END:
+    level--;
     return;
 }
