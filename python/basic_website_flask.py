972e0233440b74ecec4ce46a6cdd9e000e7cd405
Kenneth MacDonald <kh_macdonald@yahoo.com>
2020-05-05 16:30:12 -0400

    Adding a basic python website using flask.

---
+++ python/basic_website_flask.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    #return "Hello world, by me"
+    return render_template("index.html")
+
+@app.route('/usr/<int:id>')
+def user (id) :
+    return render_template("user.html", uid=id)
+
+if __name__=='__main__':
+    app.run(debug=True)
