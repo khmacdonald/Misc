50b6df26d976225bc012dcc7905bdedb164dd35d
Kenneth MacDonald <kh_macdonald@yahoo.com>
2020-12-09 23:09:02 -0500

    Adding comments and Makefile.

--- algorithms/lexical_permutation/lex_perm.c
+++ algorithms/lexical_permutation/lex_perm.c
@@ -1,120 +1,33 @@
 #include <stdio.h>
 
 #include "common.h"
-#include "print_statements.h"
 
-
-int factorial(int n);
-int next_lex_perm(int n, int * a);
-
-int test01(int argc, char ** argv);
-int test02(int argc, char ** argv);
-int test03(int argc, char ** argv);
+static int  find_k  (int n, int * a );
+static int  find_m  (int n, int * a, int k);
+static void reverse (int * a, int k, int n );
+static void swap    (int * a, int k, int m);
 
 /* ------------------------------------------------------ */
 
-int main ( int argc, char ** argv ) {
-    test03(argc, argv);
-}
-
-/* ------------------------------------------------------ */
-
-int test03 ( int argc, char ** argv ) {
-    const int n = 3;
-    int arr[n] = {0, 1, 2};
-    int cnt, max_cnt = 10;
-
-    c_print_array_int(arr, n, 1);
-    for ( cnt=0; cnt<max_cnt; ++cnt ) {
-        next_lex_perm(n, arr);
-        c_print_array_int(arr, n, 1);
-    }
-    return 0;
-}
-
-
-int test02 ( int argc, char ** argv ) {
-    const int n = 3;
-    int arr[n] = {0, 1, 2};
-
-    c_print_array_int(arr, n, 1);
-    while (1) {
-        if ( next_lex_perm(n, arr) ) {
-            break;
-        }
-        c_print_array_int(arr, n, 1);
-    }
-    return 0;
-}
-
-
-int test01 ( int argc, char ** argv ) {
-    const int n = 7;
-    int arr[n] = {0, 1, 2, 5, 3, 3, 0};
-
-    c_print_array_int(arr, n, 1);
-
-    next_lex_perm(n, arr);
-    c_print_array_int(arr, n, 1);
-    next_lex_perm(n, arr);
-    c_print_array_int(arr, n, 1);
-    next_lex_perm(n, arr);
-    c_print_array_int(arr, n, 1);
-    next_lex_perm(n, arr);
-    c_print_array_int(arr, n, 1);
-
-    return 0;
-}
-
-/* ------------------------------------------------------ */
-
-int factorial(int n){
+/**
+ * @brief Compute n!
+ * 
+ * @return Returns n!.  Could over flow.
+ */
+unsigned int lex_factorial(unsigned int n){ /**< The integer to compute n!. */
     if ( n<2 ) {
         return 1;
     }
-    return n * factorial(n-1);
+    return n * lex_factorial(n-1);
 }
 
-static int find_k(int n, int * a ) {
-    /* Find the first non-ascending number starting at the end of the array */
-    int k = n-1;
-    while ( k>0 ) {
-        if (a[k-1] < a[k] ) {
-            return k-1;
-        }
-        k--;
-    }
-    return -1;
-}
-
-static int find_m(int n, int * a, int k) {
-    int m=n-1, a_k = a[k];
-    
-    while ( m>k ) {
-        if ( a[m] > a_k ) {
-            return m;
-        }
-        m--;
-    }
-    return -1;
-}
-
-static void swap(int * a, int k, int m) {
-    int tmp = a[k];
-    a[k] = a[m];
-    a[m] = tmp;
-}
-
-static void reverse(int * a, int k, int n ){
-    int half = (n-k)/2;
-    int m;
-
-    for ( m=0; m<half; ++m ) {
-        swap(a, k+m, n-1-m);
-    }
-}
-
-int next_lex_perm(int n, int * a) {
+/**
+ * @brief Computes the next lexicographic permutation, in place.
+ * 
+ * @return TBD.
+ */
+perm_t next_lex_perm(int n,     /**< The length of the array. */
+                     int * a) { /**< The array to permute. */
     static int cnt = 0;
     int k, m;
 
@@ -143,4 +56,50 @@ int next_lex_perm(int n, int * a) {
     reverse(a, k+1, n);
 
     return 0;
+}
+
+/* ------------------------------------------------------ */
+
+/* Find the first non-ascending number starting at the end of the array */
+static int find_k(int n, int * a ) {
+    int k = n-1;
+    while ( k>0 ) {
+        if (a[k-1] < a[k] ) {
+            return k-1;
+        }
+        k--;
+    }
+    return -1;
+}
+
+/* Find the first number greater than a[k] starting at the end of the array */
+static int find_m(int n, int * a, int k) {
+    int m=n-1, a_k = a[k];
+    
+    while ( m>k ) {
+        if ( a[m] > a_k ) {
+            return m;
+        }
+        m--;
+    }
+    return -1;
+}
+
+/* Swaps a[k] and a[m] */
+static void swap(int * a, int k, int m) {
+    int tmp = a[k];
+    a[k] = a[m];
+    a[m] = tmp;
+}
+
+/* Reverses the last n-k elements of an array:
+   (a[0] ... a[k]   a[k+1] ... a[n-2] a[n-1]) 
+-> (a[0] ... a[n-1] a[n-2] ... a[k+1] a[k]) */
+static void reverse(int * a, int k, int n ){
+    int half = (n-k)/2;
+    int m;
+
+    for ( m=0; m<half; ++m ) {
+        swap(a, k+m, n-1-m);
+    }
 }
